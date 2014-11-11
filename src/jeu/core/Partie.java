cbefbca49ffcfcaec4d4070de7edbe7387b77bf5
Arnaud Merlier <arnaud.merlier@gmail.com>
2014-11-11 13:04:35 +0100

    save

--- src/jeu/core/Partie.java
+++ src/jeu/core/Partie.java
@@ -6,20 +6,27 @@
 package jeu.core;
 
 import java.util.Observable;
+import java.util.Random;
 import jeu.persist.Joueur;
+import jeu.persist.factory.JdbcKit;
+import jeu.persist.factory.PersistKit;
+import jeu.persist.factory.SrKit;
+import jeu.persist.factory.XmlKit;
+import jeu.persist.scores.HighScore;
 import jeu.ui.LancerForm2;
 import jeu.ui.MenuForm;
 import jeu.ui.VueDe;
 import jeu.ui.VueJoueur;
 import jeu.ui.VuePartie;
 
-public class Partie extends Observable{
+public class Partie extends Observable {
+
     private final int NB_TOURS = 10;
     private int nbTours = 10;
-    private De de1,de2;
-    private Joueur j1,j2,joueurActif;
-    private VueJoueur vj1,vj2;
-    private VueDe vd1,vd2;
+    private De de1, de2;
+    private Joueur j1, j2, joueurActif;
+    private VueJoueur vj1, vj2;
+    private VueDe vd1, vd2;
     private VuePartie vp;
 
     /**
@@ -28,9 +35,9 @@ public class Partie extends Observable{
     private Partie() {
         de1 = new De(1);
         de2 = new De(2);
-        j1 = new Joueur("toto",0);     
-        j2 = new Joueur("titi",0);     
-        
+        j1 = new Joueur("toto", 0);
+        j2 = new Joueur("titi", 0);
+
         vj1 = new VueJoueur();
         vj2 = new VueJoueur();
         j1.addObserver(vj1);
@@ -42,7 +49,7 @@ public class Partie extends Observable{
         this.joueurActif = j1;
         vp = new VuePartie();
         this.addObserver(vp);
-        
+
     }
 
     private static Partie INSTANCE = null;
@@ -53,45 +60,68 @@ public class Partie extends Observable{
         }
         return INSTANCE;
     }
-    
-    public void lancerLesDes(){
+
+    public void lancerLesDes() {
         if (nbTours == 0) {
             return;
         }
         int v1 = de1.lancer();
-        int v2 = de2.lancer(); 
-        if (v1+v2 == 10){
+        int v2 = de2.lancer();
+        if (v1 + v2 == 10) {
             marquerPoints(10);
         }
         nbTours--;
         setChanged();
         notifyObservers(nbTours);
     }
-    
-    public Integer getNbTours(){
+
+    public Integer getNbTours() {
         return nbTours;
     }
-    
-    public Joueur getJoueurActif(){
+
+    public Joueur getJoueurActif() {
         return this.joueurActif;
     }
-    
-    public void marquerPoints(int score){
+
+    public void marquerPoints(int score) {
         joueurActif.addScore(score);
     }
-    
-    public void changerJoueurActif(){
-        if (joueurActif.equals(j1)){
+
+    public void changerJoueurActif() {
+        if (joueurActif.equals(j1)) {
             joueurActif = j2;
-        }
-        else {
+        } else {
             joueurActif = j1;
         }
         nbTours = NB_TOURS;
         setChanged();
         notifyObservers(NB_TOURS);
     }
-    
+
+    /*
+     Fonction tres utile de sauvegarde via un moyen aléatoire
+     ce qui fait qu'on ne pourra pas lister les scores.
+     */
+    public void sauvegarder() {
+        PersistKit pk;
+        HighScore highscore;
+
+        Random r = new Random();
+        int max = 300;
+        int min = 1;
+        int alea = r.nextInt(max - min + 1);
+        if (alea <= 100) {
+            pk = new JdbcKit();
+        } else if (alea > 100 && alea <= 200) {
+            pk = new SrKit();
+        } else {
+            pk = new XmlKit();
+        }
+        highscore = pk.makeKit();
+        highscore.ecrire();
+
+    }
+
     public static void main(String[] args) {
         MenuForm mf = new MenuForm();
         mf.setLocationRelativeTo(null);
--- src/jeu/ui/LancerForm2.java
+++ src/jeu/ui/LancerForm2.java
@@ -43,6 +43,7 @@ public class LancerForm2 extends javax.swing.JFrame {
         txtDe2 = new javax.swing.JTextField();
         jPanel1 = new javax.swing.JPanel();
         lblScore = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -110,41 +111,56 @@ public class LancerForm2 extends javax.swing.JFrame {
                 .addContainerGap(50, Short.MAX_VALUE))
         );
 
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(84, 84, 84)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(jButton2))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(btnLancerDe)
-                        .addGap(18, 18, 18)
-                        .addComponent(btnChangerJoueur)
-                        .addGap(0, 0, Short.MAX_VALUE))
+                        .addGap(84, 84, 84)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addComponent(jButton2))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btnLancerDe)
+                                .addGap(18, 18, 18)
+                                .addComponent(btnChangerJoueur)
+                                .addGap(0, 0, Short.MAX_VALUE))))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel1)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(lblJoueurActif))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jLabel4)
-                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(txtDe1, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                    .addComponent(jLabel6))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addGap(84, 84, 84)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jLabel5)
+                                        .addComponent(jLabel1)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(txtDe2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                    .addComponent(txtTotal, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                        .addComponent(lblJoueurActif))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                            .addGroup(layout.createSequentialGroup()
+                                                .addComponent(jLabel4)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(txtDe1, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                            .addComponent(jLabel6))
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addGroup(layout.createSequentialGroup()
+                                                .addComponent(jLabel5)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(txtDe2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                            .addComponent(txtTotal, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(25, 25, 25)
+                                .addComponent(jButton1)))
                         .addGap(53, 53, 53)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
@@ -175,7 +191,9 @@ public class LancerForm2 extends javax.swing.JFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel6)
                             .addComponent(txtTotal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(85, 85, 85)
+                        .addGap(29, 29, 29)
+                        .addComponent(jButton1)
+                        .addGap(33, 33, 33)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(btnLancerDe)
                             .addComponent(btnChangerJoueur)))
@@ -199,6 +217,10 @@ public class LancerForm2 extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         System.exit(0);
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        partie.sauvegarder();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     public static LancerForm2 getForm() {
         return LancerForm2.lf;
@@ -233,6 +255,7 @@ public class LancerForm2 extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnChangerJoueur;
     private javax.swing.JButton btnLancerDe;
+    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
